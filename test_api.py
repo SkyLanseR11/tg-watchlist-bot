b4c79c7162f8e973d5d76a521095070ceb325fcf
SkyLanseR11 <skylansick@gmail.com>
2025-05-16 16:31:42 +0300

    Map location of MKS with using API

---
+++ test_api.py
@@ -0,0 +1,18 @@
+import requests
+
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)  # При другом коде ответа выводим этот код
+
+coordinates = response.json()["iss_position"]
+print(coordinates)
+formatted_coordinates = f"{coordinates['longitude']}%2C{coordinates['latitude']}"
+print(formatted_coordinates)
+map_url = f"https://yandex.ru/maps/?ll={formatted_coordinates}"
+print(map_url)
